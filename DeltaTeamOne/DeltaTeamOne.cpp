cc598f988bab500d837161a9f45192ac9b9f8337
Aron Gaspic <arongaspic1@gmail.com>
2020-04-25 09:09:07 +0200

    Init

---
+++ DeltaTeamOne/DeltaTeamOne.cpp
@@ -0,0 +1,12 @@
+// All rights reserved. Acodeon j.d.o.o.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello to our Delta team one game!\nJK - we will open our website for you. :)\n\n\n";
+	std::string op = std::string("start ").append("https://acodeon.net/");
+	system(op.c_str());
+	std::cin;
+}
+
